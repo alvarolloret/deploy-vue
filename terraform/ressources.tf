cd0f30a509459dc24d94488cc5fd3312a2b25666
Alavaro <alvaro@oneragtime.com>
2021-08-26 19:02:10 +0200

    nginx running and working :)

--- terraform/ressources.tf
+++ terraform/ressources.tf
@@ -6,7 +6,7 @@ resource "hcloud_ssh_key" "default" {
 
 resource "hcloud_server" "test" {
   name = "test"
-  image = "debian-9"
+  image = "ubuntu-20.04"
   server_type = "cx11"
   ssh_keys = ["${hcloud_ssh_key.default.name}"]
   
@@ -25,4 +25,13 @@ resource "godaddy_domain_record" "gd-developertoentrepreneur" {
     priority = 1
   }
 
+}
+
+resource "local_file" "foo" {
+    content     = <<-EOT
+      [ubuntu
+      ]
+      ${hcloud_server.test.ipv4_address} ansible_user=root
+    EOT
+    filename = "${path.module}/../ansible/hosts"
 }
