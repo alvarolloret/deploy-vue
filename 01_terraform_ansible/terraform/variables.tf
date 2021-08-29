74c34f8d4da65e288eb98dc88fbb2c9e5fc1a1de
Alavaro <alvaro@oneragtime.com>
2021-08-29 17:59:14 +0200

    Setting terraform variables from Terraform

---
+++ 01_terraform_ansible/terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "GODADDY_API_KEY" { default = "dKNjqpLmA3KM_PywjRXvUBUYKMvtDXiyL9r" }
+variable "GODADDY_API_SECRET" { default = "7zLoSpsmft3aAU8YvLAu4S" }
+
+variable "hcloud_token" { default = "sqYzDJkdVDwcMgBvzFI2yZpSzQtjZKb8Eexl3v63BZEYaf9nBzAlOTFxmwm2sPqk" }
